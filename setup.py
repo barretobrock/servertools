3a10b1743d4c72483cb499e71c4e041916f1ba77
bobrock <barret.obrock@gmail.com>
2020-05-26 08:12:16 -0500

    Remove dependency variable from setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ with open('config.yaml') as f:
 PACKAGE = package_info['REPO']['NAME']
 DESC = package_info['REPO']['DESC']
 URL = package_info['REPO']['URL']
-DEPS = package_info['REPO']['DEPS']
 here_dir = os.path.abspath(os.path.dirname(__file__))
 init_fp = os.path.join(here_dir, *[PACKAGE, '__init__.py'])
 
