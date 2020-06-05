0e395f187a553a99d68c9e3bf23b015f4e250e7a
barretobrock <barret.obrock@gmail.com>
2020-06-05 18:51:14 -0500

    Add kitchen strip to homeauto API

--- api/homeauto/homeauto_api.py
+++ api/homeauto/homeauto_api.py
@@ -32,7 +32,7 @@ def light_toggle():
     if light_name is None:
         raise ValueError('No light name selected')
     light_names = []
-    if light_name in ['garage', 'stairs']:
+    if light_name in ['garage', 'stairs', 'kit-strip']:
         # Handle both lights
         for i in [1, 2]:
             light = f'{light_name}-{i}'
