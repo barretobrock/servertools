3666c5fb61a9168fe7896846ef9bca303f1f6c77
barretobrock <barret.obrock@gmail.com>
2020-06-07 08:33:20 -0500

    Update server cron, mqtt docs, add new dependencies to requirements

--- crons/homeserv.sh
+++ crons/homeserv.sh
@@ -10,20 +10,21 @@ PY3=/home/bobrock/venvs/stools/bin/python3
 ## PIHOLE
 
 ## MYSQL
-#30 6 * * *          $PY3    $HOME/$SCRIPTS/grafana/grafana.py
+#30 6 * * *             $PY3    $HOME/$SCRIPTS/grafana/grafana.py
 # SYS DATA COLLECTION
-*/2 * * * *         $PY3    $HOME/$SCRIPTS/net/machine-connected.py -lvl debug
-0 */3 * * *         $PY3    $HOME/$SCRIPTS/net/speedtest-logger.py -lvl debug
+*/2 * * * *             $PY3    $HOME/$SCRIPTS/net/machine-connected.py -lvl debug
+0 */3 * * *             $PY3    $HOME/$SCRIPTS/net/speedtest-logger.py -lvl debug
 ## ENV DATA COLECTION
-#*/10 * * * *        $PY3    $HOME/$SCRIPTS/temps/ecobee_temps.py
-#20 19 * * *         $PY3    $HOME/$SCRIPTS/temps/frost_warning.py
-#*/10 * * * *        $PY3    $HOME/$SCRIPTS/temps/severe_weather_check.py
-#17 16 * * 1-5       $PY3    $HOME/$SCRIPTS/temps/significant_temp_change_warning.py
+#*/10 * * * *           $PY3    $HOME/$SCRIPTS/temps/ecobee_temps.py
 ## HOME AUTOMATION
-#*/10 03-22 * * *    $PY3    $HOME/$SCRIPTS/camera/amcrest_notify_zone.py
-#5 23 * * *          $PY3    $HOME/$SCRIPTS/camera/amcrest_nighttime.py
-## SLACK
-#15 */4 * * *        $PY3    $HOME/$SCRIPTS/slackbot/slack_logger.py
+*/10 03-22 * * *        $PY3    $HOME/$SCRIPTS/camera/amcrest_notify_zone.py -lvl debug
+5 23 * * *              $PY3    $HOME/$SCRIPTS/camera/amcrest_nighttime.py -lvl debug
+## SLACK NOTIFICATION/SIGNALING
+# Run Oct - March
+#20 19 * 10-12,1-3 *    $PY3    $HOME/$SCRIPTS/temps/frost_warning.py
+#17 16 * * 1-5          $PY3    $HOME/$SCRIPTS/temps/significant_temp_change_warning.py
+#*/10 * * * *           $PY3    $HOME/$SCRIPTS/temps/severe_weather_check.py
+#15 */4 * * *           $PY3    $HOME/$SCRIPTS/slackbot/slack_logger.py
 
 # Vpulse Automation
 #40 03 * * * export DISPLAY=:0; $PY3 $HOME/$SENSORS/vpulse/vpulse_auto.py -lvl debug
--- documentation/MQTT.md
+++ documentation/MQTT.md
@@ -2,11 +2,15 @@
 
 ## Topics
 I'll try to remember to lits all the MQTT topics to subscribe to here:
+ - sensors/#                    # All sensors, all levels
  - sensors/+/temp               # General temperature sensors at various locations
+ - sensors/cameras/+/status     # Status of all cameras
  - sensors/machines/+/temp      # Temps of all machines 
  - sensors/machines/+/uptime    # Uptime of all machines
  - sensors/machines/+/status    # Connection status of all machines
- - sensors/cameras/+/status     # Status of all cameras
+ - sensors/net/speed/download   # Download speed
+ - sensors/net/speed/upload     # Upload speed
+ - sensors/net/speed/ping       # Server ping
  
 ## Actions
  - Subscribing to a topic
