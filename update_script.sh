226d02a8acee1e602ecded50129a63f7ff79505e
Bobrock <barret.obrock@gmail.com>
2020-06-26 19:07:30 -0500

    Refactor package management scripts to rely on centralized repo

--- update_script.sh
+++ update_script.sh
@@ -1,58 +1,6 @@
 #!/usr/bin/env bash
-#/      --update_script.sh--
-#/  Pulls changes from remote master and then updates the local python package
-#/
-#/  Usage: update_script.sh [options]
-#/
-#/  Options
-#/      -s|--skip-deps                      Skips update of dependencies.
-#/      -v|--version                        Prints script name & version.
-#/
+# Calls the update_script file at whatever path the py-package-manager repo is in
+PPM_PATH=${HOME}/extras/py-package-manager/update_script.sh
 
-# DEFAULT VARIABLES
-# ------------------------------------------
-NAME="Repo Update Script"
-VERSION="0.0.3"
-SKIP_DEPS=0
-
-# Import common variables / functions
-source ./common.sh
-eval $(parse_yaml config.yaml)
-
-NODEPS_FLAG=''
-if [[ "${SKIP_DEPS}" == "1" ]];
-then
-    echo "Not installing dependencies"
-    NODEPS_FLAG="--no-deps"
-fi
-
-# GIT PULL
-# ------------------------------------------
-announce_section "Pulling update from git repo"
-(cd ${REPO_DIR} && git pull origin master)
-
-# PY PACKAGE UPDATE
-# ------------------------------------------
-announce_section "Updating custom dependencies"
-# Update dependencies first in case they have an outdated requirement
-# Slacktools
-${REPO_VENV} -m pip install -e ${REPO_DEP1} --upgrade ${NODEPS_FLAG}
-# Kavalkilu
-${REPO_VENV} -m pip install -e ${REPO_DEP2} --upgrade ${NODEPS_FLAG}
-
-# Then update the python package locally
-announce_section "Beginning update of ${REPO_NAME}"
-${REPO_VENV} -m pip install -e ${REPO_GIT_URL} --upgrade ${NODEPS_FLAG}
-
-# CRON UPDATE
-# --------------
-# Apply cronjob changes, if any.
-announce_section "Checking for crontab updates for ${HOSTNAME}"
-CRON_DIR=${REPO_DIR}/crons
-CRON_FILE=${CRON_DIR}/${HOSTNAME}.sh
-SUDO_CRON_FILE=${CRON_DIR}/su-${HOSTNAME}.sh
-
-[[ -f ${CRON_FILE} ]] && echo "Applying cron file." && crontab ${CRON_FILE} || echo "No cron file."
-[[ -f ${SUDO_CRON_FILE} ]] && echo "Applying sudo cron file." && sudo crontab ${SUDO_CRON_FILE} || echo "No sudo cron file."
-
-announce_section "Process completed"
+ADDL="${@}" # Option for passing additional commands onwards
+sh ${PPM_PATH} --config $(pwd)/config.yaml ${ADDL}
