0c1683be9de00caa01fcecaf98a3f10cdabd0542
Bobrock <barret.obrock@gmail.com>
2020-12-05 22:05:45 -0600

    Adjust timing for humidifier

--- crons/tinyserv.sh
+++ crons/tinyserv.sh
@@ -9,28 +9,20 @@ PY3=/home/bobrock/venvs/stools/bin/python3
 #32 3 20 * *            $PY3    $HOME/$SCRIPTS/log_remover.py -lvl debug
 ## PIHOLE
 
-## MYSQL
-#30 6 * * *             $PY3    $HOME/$SCRIPTS/grafana/grafana.py
 # SYS DATA COLLECTION
 */2 * * * *             $PY3    $HOME/$SCRIPTS/net/machine-connected.py
 0 */3 * * *             $PY3    $HOME/$SCRIPTS/net/speedtest-logger.py
 ## ENV DATA COLECTION
-#*/10 * * * *           $PY3    $HOME/$SCRIPTS/weather/ecobee_temps.py
 */10 * * * *            $PY3    $HOME/$SCRIPTS/weather/local_weather.py
 0 * * * *               $PY3    $HOME/$SCRIPTS/weather/forecast_collector.py
 ## HOME AUTOMATION
-#*/5 03-22 * * *         $PY3    $HOME/$SCRIPTS/camera/amcrest_notify_zone.py -lvl debug
-#5 23 * * *              $PY3    $HOME/$SCRIPTS/camera/amcrest_nighttime.py -lvl debug
 02 * * * *              $PY3    $HOME/$SCRIPTS/camera/motion_alerts.py -c ac-v2lis -lvl debug
-0 */2 * * *              $PY3    $HOME/$SCRIPTS/plants/mushroom-grow-toggly.py -lvl debug
+0 */2 * * *             $PY3    $HOME/$SCRIPTS/plants/mushroom-grow-toggle.py -lvl debug
 ## SLACK NOTIFICATION/SIGNALING
-20 19 * 10-12,1-3 *     $PY3    $HOME/$SCRIPTS/weather/frost_warning.py -lvl debug # Run Oct - March only
+20 19 * 10-12,1-3 *     $PY3    $HOME/$SCRIPTS/weather/frost_warning.py -lvl debug # Run Oct-March
 17 16 * * 0-5           $PY3    $HOME/$SCRIPTS/weather/daily_weather_and_sig_temp_warn.py -lvl debug
 */10 * * * *            $PY3    $HOME/$SCRIPTS/weather/severe_weather_check.py -lvl debug
 17 10 * * *             $PY3    $HOME/$SCRIPTS/weather/moon_phase.py
-#15 */4 * * *           $PY3    $HOME/$SCRIPTS/slackbot/slack_logger.py
-#21 9 * * *              $PY3    $HOME/$SCRIPTS/slackbot/emoji_scraper.py
-#19 10-14 * * 1-5        $PY3    $HOME/$SCRIPTS/slackbot/memeraker.py -lvl debug
 27 8 * * *              $PY3    $HOME/$SCRIPTS/slackbot/word-of-the-day.py -lvl debug
 
 # Vpulse Automation
--- scripts/plants/mushroom-grow-toggle.py
+++ scripts/plants/mushroom-grow-toggle.py
@@ -1,10 +1,24 @@
+import time
 from kavalkilu import LogWithInflux
 from servertools import HueBulb
 
+INTERVAL_MINS = 30
+ON_TIME_WEIGHT = 1 / 3
+OFF_TIME_WEIGHT = 1 - ON_TIME_WEIGHT
+
 logg = LogWithInflux('mushroom-grow-toggle')
 h = HueBulb('mushroom-plug')
+n_rounds = int(60 / INTERVAL_MINS)
 
-logg.debug('Toggling humidifier...')
-h.toggle()
+on_period_secs = INTERVAL_MINS * 60 * ON_TIME_WEIGHT
+off_period_secs = INTERVAL_MINS * 60 * OFF_TIME_WEIGHT
+
+for x in range(0, n_rounds):
+    h.turn_on()
+    logg.debug(f'Turning on humidifier. Waiting {on_period_secs / 60:.0f} mins')
+    time.sleep(on_period_secs)
+    logg.debug(f'Turning off humidifier. Waiting {off_period_secs / 60:.0f} mins...')
+    h.turn_off()
+    time.sleep(off_period_secs)
 
 logg.close()
