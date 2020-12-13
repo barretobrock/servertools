b611781e99a0ae3317b029f527fe0d7c486c4bef
Bobrock <barret.obrock@gmail.com>
2020-12-12 22:35:35 -0600

    Update package maintenance scripts

---
+++ _auto_config.sh
@@ -0,0 +1,12 @@
+# **This is an automatically-constructed file. See config.py for the source.**
+
+REPO_NAME="servertools"
+REPO_DIR="/home/bobrock/extras/servertools"
+GIT_URL="git+https://github.com/barretobrock/servertools.git#egg=servertools"
+VENV_PATH="${HOME}/venvs/stools/bin/python3"
+DEP_LINKS=(
+	"git+https://github.com/barretobrock/slacktools.git#egg=slacktools"
+	"git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu"
+	"git+https://github.com/barretobrock/easylogger.git#egg=easylogger"
+)
+MAIN_BRANCH="master"
--- push_changes.sh
+++ push_changes.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Calls the push_changes file at whatever path the py-package-manager repo is in
-PPM_PATH=${HOME}/extras/py-package-manager/push_changes.sh
+PPM_PATH=../py-package-manager/push_changes.sh
 
-ADDL="${@}" # Option for passing additional commands onwards
-sh ${PPM_PATH} --config $(pwd)/config.yaml ${ADDL}
+if [[ ! -z "${PPM_PATH}" ]]
+then
+  ADDL="${@}" # Option for passing additional commands onwards
+  sh ${PPM_PATH} --config ./config.py ${ADDL}
+else
+  echo -e "The py-package-manager repo is not in the expected path: ${PPM_PATH}\nAborting process..." && exit 1
+fi
