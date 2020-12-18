4ecde889a7840509394704a138e12c88b163178a
Bobrock <barret.obrock@gmail.com>
2020-12-18 04:47:16 -0600

    Reorganize crontab, add timelapse scripts

--- crons/tinyserv.sh
+++ crons/tinyserv.sh
@@ -4,21 +4,26 @@
 SCRIPTS=extras/servertools/scripts
 PY3=/home/bobrock/venvs/stools/bin/python3
 
-# LOG ANALYSIS
-#0 */4 * * *            $PY3    $HOME/$SCRIPTS/log_reader.py
-#32 3 20 * *            $PY3    $HOME/$SCRIPTS/log_remover.py -lvl debug
 ## PIHOLE
 
-# SYS DATA COLLECTION
+## DATA COLLECTION
+## Network
 */2 * * * *             $PY3    $HOME/$SCRIPTS/net/machine-connected.py
 0 */3 * * *             $PY3    $HOME/$SCRIPTS/net/speedtest-logger.py
-## ENV DATA COLECTION
+## Climate
 */10 * * * *            $PY3    $HOME/$SCRIPTS/weather/local_weather.py
 0 * * * *               $PY3    $HOME/$SCRIPTS/weather/forecast_collector.py
+
 ## HOME AUTOMATION
+### Camera
 02 * * * *              $PY3    $HOME/$SCRIPTS/camera/reolink_motion_alerts.py -c re-v2lis -lvl debug
 15 * * * *              $PY3    $HOME/$SCRIPTS/camera/reolink_motion_alerts.py -c re-eesuks -lvl debug
+*/30 * * * *            $PY3    $HOME/$SCRIPTS/camera/timelapse.py -c ac-allr6du -lvl debug
+*/30 * * * *            $PY3    $HOME/$SCRIPTS/camera/timelapse.py -c ac-yler6du -lvl debug
+*/30 * * * *            $PY3    $HOME/$SCRIPTS/camera/timelapse.py -c re-v2lis -lvl debug
+### Other
 0 */2 * * *             $PY3    $HOME/$SCRIPTS/plants/mushroom-grow-toggle.py -lvl debug
+
 ## SLACK NOTIFICATION/SIGNALING
 20 19 * 10-12,1-3 *     $PY3    $HOME/$SCRIPTS/weather/frost_warning.py -lvl debug # Run Oct-March
 17 16 * * 0-5           $PY3    $HOME/$SCRIPTS/weather/daily_weather_and_sig_temp_warn.py -lvl debug
@@ -26,7 +31,7 @@ PY3=/home/bobrock/venvs/stools/bin/python3
 17 10 * * *             $PY3    $HOME/$SCRIPTS/weather/moon_phase.py
 27 8 * * *              $PY3    $HOME/$SCRIPTS/slackbot/word-of-the-day.py -lvl debug
 
-## SELENIUM-BASED
+## SELENIUM
 ### Apt price scraping
 05 05 * * * export DISPLAY=:0; $PY3 $HOME/$SCRIPTS/net/apt_prices.py -lvl debug
 ### Vpulse Automation
