86d7c4dfe1e38d9cb436b6e745698541397d5a19
Bobrock <barret.obrock@gmail.com>
2020-12-22 19:13:38 -0600

    Move logging object in hosts api to avoid connection issues

--- api/hosts/main_api.py
+++ api/hosts/main_api.py
@@ -7,7 +7,6 @@ from api.hosts.hosts import hosts
 from api.hosts.keys import keys
 
 
-logg = LogWithInflux('hosts-api')
 app = Flask(__name__)
 for blueprint in [keys, hosts]:
     app.register_blueprint(blueprint)
@@ -27,3 +26,5 @@ if __name__ == '__main__':
     secret_key = get_secret_file('FLASK_SECRET')
     app.secret_key = secret_key
     app.run(host='0.0.0.0', port='5002')
+    # Instantiate log here, as the hosts API is requested to comunicate with influx
+    log = LogWithInflux('hosts_api')
