4b14eb7d57dd0a971f0c3ce43c15048b33a33269
Bobrock <barret.obrock@gmail.com>
2020-12-25 15:16:09 -0600

    Add easier way of sending collated timelapse videos

--- scripts/camera/combine_timelapse.py
+++ scripts/camera/combine_timelapse.py
@@ -1,9 +1,11 @@
 """For joining timelapse shots"""
 import os
 from moviepy.editor import ImageSequenceClip
-from kavalkilu import Path, Hosts, HOME_SERVER_HOSTNAME
+from kavalkilu import Path, LogWithInflux
+from servertools import SlackComm
 
 
+log = LogWithInflux('timelapse_combi')
 p = Path()
 tl_dir = p.easy_joiner(p.data_dir, 'timelapse')
 fnames = {}
@@ -12,8 +14,16 @@ for dirpath, _, filenames in os.walk(tl_dir):
     if dirname != 'timelapse':
         fnames[os.path.basename(dirpath)] = filenames
 
+files = []
 # Begin combining shots
 for k, v in fnames.items():
     full_paths = sorted([os.path.join(tl_dir, *[k, x]) for x in v])
     clip = ImageSequenceClip(full_paths, fps=30)
-    clip.write_videofile(os.path.join(tl_dir, f'concat_{k}.mp4'))
+    fpath = os.path.join(tl_dir, f'concat_{k}.mp4')
+    clip.write_videofile(fpath)
+    files.append(fpath)
+
+scom = SlackComm(parent_log=log)
+for file in files:
+    scom.st.upload_file('kaamerad', file, os.path.basename(file))
+
