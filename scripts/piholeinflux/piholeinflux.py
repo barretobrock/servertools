fc8527284b96130b04e6dcb5529bb51584f6bc95
Bobrock <barret.obrock@gmail.com>
2021-01-03 17:27:32 -0600

    Load PiHole to Influx ETL service

--- scripts/piholeinflux/piholeinflux.py
+++ scripts/piholeinflux/piholeinflux.py
@@ -1,46 +1,74 @@
 import os
 from datetime import datetime as dt, timedelta
-from kavalkilu import Log, DateTools, InfluxDBLocal, InfluxDBNames, InfluxTblNames
-from servertools import SlackComm, SQLLiteLocal
+from kavalkilu import LogWithInflux, DateTools, InfluxDBLocal, InfluxDBPiHole, SQLLiteLocal, Hosts
+from servertools import SlackComm
 
 
-logg = Log('pihole_etl', log_to_db=True)
+logg = LogWithInflux('pihole_etl', log_to_db=True)
 sc = SlackComm()
+hosts = {x['ip']: x['name'] for x in Hosts().get_all_hosts()}
 datetools = DateTools()
 
-FTL_DB_PATH = os.path.join(os.path.expanduser('~'), *['Downloads', 'pihole-FTL.db'])
+FTL_DB_PATH = os.path.join('/etc', *['pihole', 'pihole-FTL.db'])
 sqll = SQLLiteLocal(FTL_DB_PATH)
 
 INTERVAL_MINS = 60
-end = dt.now()
-start = (end - timedelta(minutes=INTERVAL_MINS)).replace(second=0, microsecond=0)
+end = dt.now().replace(second=0, microsecond=0)
+start = (end - timedelta(minutes=INTERVAL_MINS))
+unix_start = datetools.dt_to_unix(start)
+unix_end = datetools.dt_to_unix(end)
 
-query = """
+query = f"""
     SELECT
         client
         , domain
-        , status
         , CASE
-            WHEN status = 0 THEN 'unknown'
-            WHEN status = 1 OR status > 3 THEN 'blocked'
-            WHEN status = 2 THEN 'forwarded'
-            WHEN status = 3 THEN 'known from cache' 
-            ELSE 'unknown'
-        END AS status_type
+            WHEN status = 0 THEN 'UNKNOWN'
+            WHEN status = 1 OR status > 3 THEN 'BLOCKED'
+            WHEN status = 2 OR status = 3 THEN 'ALLOWED' 
+            ELSE 'UNKNOWN'
+        END AS query_status
+        , CASE
+            WHEN status = 0 THEN 'NO ANSWER'
+            WHEN status = 1 THEN 'IN GRAVITY'
+            WHEN status = 2 THEN 'FORWARDED'
+            WHEN status = 3 THEN 'FROM CACHE'
+            WHEN status = 4 THEN 'REGEX MATCH'
+            WHEN status = 5 THEN 'EXACT MATCH'
+            WHEN status = 6 THEN 'UPSTREAM KNOWN IP'
+            WHEN status = 7 THEN 'UPSTREAM ALL IP'
+            WHEN status = 8 THEN 'UPSTREAM NXDOMAIN'
+            WHEN status = 9 THEN 'IN GRAVITY CNAME'
+            WHEN status = 10 THEN 'REGEX MATCH CNAME'
+            WHEN status = 11 THEN 'EXACT MATCH CNAME' 
+            ELSE 'UNKNOWN'
+        END AS status_info
         , timestamp
-        , COUNT(DOMAIN)
+        , COUNT(DOMAIN) AS query_cnt
     FROM
         queries
     WHERE
         domain != ''
-        --AND timestamp BETWEEN '{}' AND '{}'
+        AND timestamp BETWEEN '{unix_start}' AND '{unix_end}'
     GROUP BY
         timestamp
         , client
         , domain
-        , status_type
-        , status
-    LIMIT 10
-""".format(datetools.dt_to_unix(start), datetools.dt_to_unix(end))
+        , query_status
+        , status_info
+"""
 
 df = sqll.read_sql(query)
+logg.debug(f'Returned {df.shape[0]} rows of data.')
+# Convert unix back to dt
+df['timestamp'] = df['timestamp'].apply(lambda x: datetools.unix_to_dt(x))
+# Lookup all known clients
+df['client'] = df['client'].replace(hosts)
+
+# Feed data into influx
+influx = InfluxDBLocal(InfluxDBPiHole.QUERIES)
+logg.debug('Writing dataframe to influx table.')
+influx.write_df_to_table(df, tags=['client', 'domain', 'query_status', 'status_info'],
+                         value_cols=['query_cnt'], time_col='timestamp')
+
+logg.close()
