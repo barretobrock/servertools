b63ddb3e5516ebff4c06bd719f9f346c157aa8f2
Bobrock <barret.obrock@gmail.com>
2021-01-31 09:48:29 -0600

    Improve finance class testing

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -1,8 +1,8 @@
 import os
 import unittest
 from datetime import datetime
-from kavalkilu import LogWithInflux
-from servertools import GNUCash, InvestmentResearch
+from kavalkilu import LogWithInflux, Keys
+from servertools import GNUCash, EdgarCollector
 
 
 class TestGNUCash(unittest.TestCase):
@@ -10,16 +10,22 @@ class TestGNUCash(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        fpath = os.path.join(os.path.expanduser('~'), *['Dropbox', 'Finance', 'GNUCash_2020', 'GNUCash2020_SQL.gnucash'])
-        cls.gcash = GNUCash(fpath)
+        props = Keys().get_key('gnucash-report')
+        cls.gcash = GNUCash(props.get('path'))
 
     def setUp(self) -> None:
-        self.start = datetime(2020, 12, 1)
-        self.end = datetime(2020, 12, 31)
+        self.start = datetime.now().replace(day=1)
+        self.end = datetime.now()
 
     def test_transaction_filter(self):
         """Test filtering transactions for the month provided"""
         df = self.gcash.filter_transactions(start=self.start, end=self.end)
+        self.assertTrue(not df.empty)
+
+        # Test more robust filtering
+        df = self.gcash.filter_transactions(start=self.start, end=self.end, filter_types=['Expenses'],
+                                            filter_desc=['Amazon.com Order'],
+                                            groupby_list=['type', 'desc', 'account'])
         self.assertTrue(not df.empty)
 
     def test_budget_filter(self):
@@ -29,8 +35,9 @@ class TestGNUCash(unittest.TestCase):
 
     def test_budget_v_actual(self):
         """Test filtering transactions for the month provided"""
-        month = datetime(2020, 12, 20)
-        df = self.gcash.generate_monthly_budget_v_actual(month, 'Q4 2020')
+        month = datetime.now()
+        df = self.gcash.generate_monthly_budget_v_actual(month, 'Budget 2021',
+                                                         filter_types=['Income', 'Expenses'])
         self.assertTrue(not df.empty)
 
 
@@ -40,11 +47,11 @@ class TestInvestmentResearch(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.log = LogWithInflux('invest-test')
-        cls.invest = InvestmentResearch(log=cls.log)
+        cls.ed = EdgarCollector(start_year=2017, parent_log=cls.log)
 
     def test_stock_data_collection(self):
         tickers = ['PG', 'MMM']
-        ratios = self.invest.collect_ratios(tickers)
+        df = self.ed.get_data_for_stock_tickers(tickers)
 
 
 if __name__ == '__main__':
