c0dce25159590e50de4533894298669ed695beee
Bobrock <barret.obrock@gmail.com>
2021-06-12 19:32:19 -0500

    Remove unnecessary requirements pins, adjust crontab

--- crons/tinyserv.sh
+++ crons/tinyserv.sh
@@ -17,8 +17,8 @@ PY3=/home/bobrock/venvs/stools/bin/python3
 
 ## HOME AUTOMATION
 ### Camera
-02 * * * *              $PY3    $HOME/$SCRIPTS/camera/reolink_motion_alerts.py -c re-v2lis -lvl debug
-15 * * * *              $PY3    $HOME/$SCRIPTS/camera/reolink_motion_alerts.py -c re-eesuks -lvl debug
+#02 * * * *              $PY3    $HOME/$SCRIPTS/camera/reolink_motion_alerts.py -c re-v2lis -lvl debug
+#15 * * * *              $PY3    $HOME/$SCRIPTS/camera/reolink_motion_alerts.py -c re-eesuks -lvl debug
 #*/5 * * * *             $PY3    $HOME/$SCRIPTS/camera/timelapse.py -c ac-allr6du -lvl debug
 #*/5 * * * *             $PY3    $HOME/$SCRIPTS/camera/timelapse.py -c ac-yler6du -lvl debug
 #*/5 * * * *             $PY3    $HOME/$SCRIPTS/camera/timelapse.py -c re-v2lis -lvl debug
@@ -27,12 +27,12 @@ PY3=/home/bobrock/venvs/stools/bin/python3
 
 ## SLACK NOTIFICATION/SIGNALING
 20 19 * 10-12,1-3 *     $PY3    $HOME/$SCRIPTS/weather/frost_warning.py -lvl debug # Run Oct-March
-17 16 * * 0-5           $PY3    $HOME/$SCRIPTS/weather/daily_weather_and_sig_temp_warn.py -lvl debug
+#17 16 * * 0-5           $PY3    $HOME/$SCRIPTS/weather/daily_weather_and_sig_temp_warn.py -lvl debug
 */10 * * * *            $PY3    $HOME/$SCRIPTS/weather/severe_weather_check.py -lvl debug
 17 10 * * *             $PY3    $HOME/$SCRIPTS/weather/moon_phase.py
 27 9 * * *              $PY3    $HOME/$SCRIPTS/slackbot/emoji_scraper.py -lvl debug
 27 8 * * *              $PY3    $HOME/$SCRIPTS/slackbot/word-of-the-day.py -lvl debug
-0 9 * * 0-5             $PY3    $HOME/$SCRIPTS/finance/stonks-ticker.py
+0 9 * * 1-5             $PY3    $HOME/$SCRIPTS/finance/stonks-ticker.py
 
 ## SELENIUM
 ### Apt price scraping
